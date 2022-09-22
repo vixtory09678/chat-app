9dd3c82e63589be08de0b93ba8c856608f01970a
vixtory09678 <robotics.za@gmail.com>
2022-09-22 11:37:35 +0700

    feat: env config

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_COOKIE_APP: process.env.NEXT_PUBLIC_COOKIE_APP
+  }
 }
 
 module.exports = nextConfig
