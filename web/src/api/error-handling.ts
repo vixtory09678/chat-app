8fd46d2b7715c259929df152c2e04d7a45f36559
vixtory09678 <robotics.za@gmail.com>
2022-09-26 05:12:58 +0700

    fix: wrong data usage

--- web/src/api/error-handling.ts
+++ web/src/api/error-handling.ts
@@ -8,13 +8,9 @@ const COOKIE_APP = publicRuntimeConfig.NEXT_PUBLIC_COOKIE_APP;
 
 export function errorHandler(
   error: any,
-  toastProvider?: (
-    message: SnackbarMessage,
-    options?: OptionsObject | undefined,
-  ) => SnackbarKey,
+  toastProvider?: (message: SnackbarMessage, options?: OptionsObject | undefined) => SnackbarKey,
 ) {
   let errorMessage = '';
-
   if (error instanceof AxiosError) {
     const status = error.response?.status;
     errorMessage = error.response?.data?.message || 'Unknown error';
@@ -28,7 +24,7 @@ export function errorHandler(
   }
 
   toastProvider
-    ? toastMessage(error.message, { variant: 'error' }, toastProvider)
+    ? toastMessage(errorMessage, { variant: 'error' }, toastProvider)
     : console.error('ErrorHandler:', errorMessage);
 }
 
@@ -42,10 +38,7 @@ export function getSimpleErrorMessage(error: any): string {
 export function toastMessage(
   message: string,
   options: OptionsObject,
-  handler: (
-    message: SnackbarMessage,
-    options?: OptionsObject | undefined,
-  ) => SnackbarKey,
+  handler: (message: SnackbarMessage, options?: OptionsObject | undefined) => SnackbarKey,
 ): SnackbarKey {
   return handler(message, {
     ...options,
