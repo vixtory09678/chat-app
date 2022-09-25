53899926028e1f4b287928f3d087b3b3d19d7d45
vixtory09678 <robotics.za@gmail.com>
2022-09-25 14:56:42 +0700

    feat: update script

--- api/src/worker/worker.service.spec.ts
+++ api/src/worker/worker.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../app.module';
 import { WorkerService } from './worker.service';
 
 describe('WorkerService', () => {
@@ -6,7 +7,7 @@ describe('WorkerService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WorkerService],
+      imports: [AppModule],
     }).compile();
 
     service = module.get<WorkerService>(WorkerService);
