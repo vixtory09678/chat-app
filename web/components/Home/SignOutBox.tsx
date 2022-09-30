4c211d9eb72473879db30942827ea32d2e0c1edb
vixtory09678 <robotics.za@gmail.com>
2022-09-30 08:55:23 +0700

    style: adjust sizing

--- web/components/Home/SignOutBox.tsx
+++ web/components/Home/SignOutBox.tsx
@@ -25,24 +25,13 @@ export function SignOutBox() {
   };
 
   return (
-    <div className="flex items-center p-1 gap-2 justify-between shadow-[0_-6px_16px_-6px_rgba(74,74,74,0.2)]">
+    <div className="flex h-16 items-center p-1 gap-2 justify-between shadow-[0_-6px_16px_-6px_rgba(74,74,74,0.2)]">
       <div className="flex items-center gap-3 flex-grow hover:cursor-pointer">
-        <img
-          src={'mock/team-profile.jpeg'}
-          height={45}
-          width={45}
-          alt=""
-          className="rounded-full"
-        />
-        <p className="w-48 text-ellipsis overflow-hidden whitespace-nowrap">
-          Wannachat Surasiang
-        </p>
+        <img src={'mock/team-profile.jpeg'} height={45} width={45} alt="" className="rounded-full" />
+        <p className="w-48 text-ellipsis overflow-hidden whitespace-nowrap">Wannachat Surasiang</p>
       </div>
 
-      <a
-        className="m-5 text-sm font-bold hover:text-blue-500 hover:cursor-pointer"
-        onClick={() => signOut()}
-      >
+      <a className="m-5 text-sm font-bold hover:text-blue-500 hover:cursor-pointer" onClick={() => signOut()}>
         LogOut
       </a>
     </div>
