57339d3417aca3758603559591bca12f4fe88071
vixtory09678 <robotics.za@gmail.com>
2022-09-30 08:52:07 +0700

    feat: support multi size

--- web/components/Home/AvatarColor.tsx
+++ web/components/Home/AvatarColor.tsx
@@ -2,16 +2,37 @@ import { UserResponse } from '../../src/api/data-contracts';
 
 interface AvatarColorProps {
   profile: UserResponse;
+  type?: 'small' | 'medium' | 'large';
 }
 
-export function AvatarColor({ profile }: AvatarColorProps) {
+export function AvatarColor({ profile, type = 'large' }: AvatarColorProps) {
+  const getSizeProfile = () => {
+    if (type === 'large') {
+      return 'h-40 w-40 border-8';
+    } else if (type === 'medium') {
+      return 'h-11 w-14 border-4';
+    } else if (type === 'small') {
+      return 'h-[40px] w-[40px] border-2';
+    }
+  };
+
+  const getSizeTextProfile = () => {
+    if (type === 'large') {
+      return 'text-5xl';
+    } else if (type === 'medium') {
+      return 'text-xl';
+    } else if (type === 'small') {
+      return 'text-xs';
+    }
+  };
+
   return (
     <>
       <div
-        className="h-40 w-40 rounded-full flex justify-center items-center border-8 border-blue-400"
+        className={`${getSizeProfile()} rounded-full flex justify-center items-center border-blue-400`}
         style={{ backgroundColor: `#${profile.profileColor}` }}
       >
-        <div className="text-black text-5xl stroke-slate-700 stroke-[5px] opacity-50">
+        <div className={`text-black ${getSizeTextProfile()} stroke-slate-700 opacity-50`}>
           {profile.displayName[0]?.toUpperCase()}
         </div>
       </div>
